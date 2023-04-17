0f54d6b002e9242117b0258937ce77ccef820140
m-heim <MaximilianHeim@protonmail.com>
2023-04-17 19:57:33 +0200

    changes to code

--- mtt/src/main.rs
+++ mtt/src/main.rs
@@ -3,9 +3,8 @@ use clap::Parser;
 /// Search for a pattern in a file and display the lines that contain it.
 #[derive(Parser)]
 struct Cli {
-    /// The pattern to look for
+    /// The subcommand
     command: String,
-    /// The path to the file to read
     debug: bool,
 }
 
