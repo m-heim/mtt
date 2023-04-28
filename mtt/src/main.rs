dd0d14abcf1f6144d09f194d8dddbf06c8d16147
m-heim <MaximilianHeim@protonmail.com>
2023-04-28 21:54:46 +0200

    change rust file

--- mtt/src/main.rs
+++ mtt/src/main.rs
@@ -5,10 +5,25 @@ use clap::Parser;
 struct Cli {
     /// The subcommand
     command: String,
-    debug: bool,
+    //debug: bool,
 }
 
 fn main() {
     println!("Thanks for using mtt! :)");
     let args = Cli::parse();
+    if args.command == "start" {
+        println!("now start job");
+    } else if args.command == "stop" {
+
+    } else if args.command == "break" {
+
+    } else if args.command == "resume" {
+        println!()
+    } else {
+        println!("Uknown command entered");
+    }
+}
+
+fn start(date: ) {
+
 }
